9fc1ec452bf501e9884d4fee7a65842d32d54fc9
Brian Ssemate <ssematebrian2067@gmail.com>
2019-04-08 19:51:07 +0300

    admin changes forms

--- sysadmin/urls.py
+++ sysadmin/urls.py
@@ -6,5 +6,7 @@ app_name = "sysadmin"
 urlpatterns = [
         url(r'^home/$',views.index,name='index'),
         url(r'^home/accounts/transactions/$',views.alltransactions,name='alltransactions'),
-        url(r'^home/accountholders/$',views.add_customer,name='add_customer')
+        url(r'^home/accounts/$',views.viewaccounts,name='viewaccounts'),
+        url(r'^home/accountholders/$',views.add_customer,name='add_customer'),
+        url(r'^home/accountholders/deposit/$',views.makedeposit,name='makedeposit')
 ]
--- sysadmin/views.py
+++ sysadmin/views.py
@@ -10,6 +10,14 @@ def alltransactions(request):
     if request.method == 'GET':
         return render(request, 'sysadmin/alltransactions.html',{})
 
+def viewaccounts(request):
+    if request.method=='GET':
+        return render(request,'sysadmin/viewaccounts.html',{})
+
 def add_customer(request):
     if request.method == 'GET':
         return render(request, 'sysadmin/addcustomer.html',{})
+
+def makedeposit(request):
+    if request.method== 'GET':
+        return render(request, 'sysadmin/makedeposit.html',{})
