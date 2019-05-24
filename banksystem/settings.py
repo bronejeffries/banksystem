f90dd3a26f5b601b2a7c24120e69b7ad84cd3395
Brian Ssemate <ssematebrian2067@gmail.com>
2019-05-24 05:01:58 +0300

    debug

--- banksystem/settings.py
+++ banksystem/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'z#-y97aza$v$+uhkyc#l@-l2)%d04zv8y7duh8nslyi46#t-nz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'banksystem.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
