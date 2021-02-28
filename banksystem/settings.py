64b501a5d37de4531f2a98dd0c2b0299adf08c0d
bronejeffries <31158018+bronejeffries@users.noreply.github.com>
2021-02-28 05:08:06 +0300

    Update settings.py

--- banksystem/settings.py
+++ banksystem/settings.py
@@ -135,9 +135,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles/')
 #django_heroku.settings(locals())
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles/'),
-]
+#STATICFILES_DIRS = [
+ #   os.path.join(BASE_DIR, 'staticfiles/'),
+#]
